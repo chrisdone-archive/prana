3f6f7b294ec77bde3aec453ace8dd867ab63deca
Chris Done <chrisdone@gmail.com>
2018-11-16 14:17:10 +0000

    Use Persist for encode/decode

--- src/Prana/Types.hs
+++ src/Prana/Types.hs
@@ -1,49 +1,76 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 -- |
 
 module Prana.Types where
 
-import           Data.ByteString (ByteString)
-import           Data.Int
-import           Data.Word
+import Data.ByteString (ByteString)
+import Data.Int
+import Data.Word
+import GHC.Generics
+import Persist (Persist(..),Get,getBytes)
 
+data Bind
+  = NonRec Var
+           Exp
+  | Rec [(Var, Exp)]
+  deriving (Generic, Show, Ord, Eq)
+instance Persist Bind
+
+instance Persist Exp
 data Exp
   = VarE Id
   | LitE Lit
   | AppE Exp Exp
   | LamE Var Exp
   | CaseE Exp Var Typ [Alt]
-  deriving (Eq, Show, Ord)
+  | TypE Typ
+  | CoercionE
+  | LetE Bind Exp
+  | CastE Exp
+  deriving (Generic, Eq, Show, Ord)
 
+instance Persist Var
 data Var = Var ByteString
-  deriving (Eq, Show, Ord)
+  deriving (Generic, Eq, Show, Ord)
 
+instance Persist Id
 data Id = Id ByteString
-  deriving (Eq, Show, Ord)
+  deriving (Generic, Eq, Show, Ord)
 
+instance Persist Typ
 data Typ = Typ ByteString
-  deriving (Eq, Show, Ord)
+  deriving (Generic, Eq, Show, Ord)
 
+instance Persist Alt
 data Alt = Alt
-  { altCon :: Con
+  { altCon :: AltCon
   , altVars :: [Var]
   , altExp :: Exp
-  } deriving (Eq, Show, Ord)
+  } deriving (Generic, Eq, Show, Ord)
 
-data Con = Con
-  deriving (Eq, Show, Ord)
+data AltCon
+  = DataAlt DataCon
+  | LitAlt Lit
+  | DEFAULT
+  deriving (Generic, Eq, Show, Ord)
+instance Persist AltCon
 
+instance Persist DataCon
+newtype DataCon = DataCon ByteString
+ deriving (Generic, Eq, Show, Ord)
+
+instance Persist Lit
 data Lit
   = Char Char
   | Str ByteString
   | NullAddr
-  | Int Int
-  | Int64 Int64
-  | Word Word
-  | Word64 Word64
-  | Float Float
-  | Double Double
+  | Int Integer
+  | Int64 Integer
+  | Word Integer
+  | Word64 Integer
+  | Float Rational
+  | Double Rational
   | Label
   | Integer Integer
-  deriving (Eq, Show, Ord)
-
-data Caf = Caf
+  deriving (Generic, Eq, Show, Ord)
