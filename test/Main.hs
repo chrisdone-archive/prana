55a25d87363c1f5915dcfea063e8304f4e0bd5b8
Chris Done <chrisdone@gmail.com>
2019-01-27 21:13:01 +0000

    Basic sanity tests for the decoder

--- test/Main.hs
+++ test/Main.hs
@@ -1,48 +1,131 @@
 {-# LANGUAGE TupleSections #-}
--- |
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Test all things on the prana side from compiling, reading .prana
+-- files, sanity checks, interpreting, etc.
 
 module Main where
 
-import           Data.ByteString (ByteString)
-import qualified Data.ByteString as S
-import           System.Directory
+import           Data.Binary.Get
+import qualified Data.ByteString.Lazy as L
+import           Prana.Decode
+import           Prana.Types
 import           System.Exit
 import           System.IO.Temp
 import           System.Process
 import           Test.Hspec
 
+-- | Main entry point.
 main :: IO ()
 main = hspec spec
 
+-- | Test suite spec.
 spec :: Spec
-spec = pure ()
+spec = describe "Compile and Decode" compileAndDecode
+
+-- | Test compiling and decoding.
+compileAndDecode :: Spec
+compileAndDecode =
+  it
+    "Compile id"
+    (do idmod <-
+          compileModules
+            [ ("Id", "module Id where id x = x")
+            , ( "On"
+              , "module On where\n\
+                \on :: (b -> b -> c) -> (a -> b) -> a -> a -> c\n\
+                \(.*.) `on` f = \\x y -> f x .*. f y")
+            ]
+        shouldBe
+          idmod
+          [ ( "Id"
+            , [ Bind
+                  { bindVar = ExportedIndex 6609
+                  , bindExp = LamE (LocalIndex 57218) (VarE (LocalIndex 57218))
+                  }
+              , Bind
+                  { bindVar = ExportedIndex 6610
+                  , bindExp =
+                      AppE
+                        (AppE
+                           (ConE ConId)
+                           (AppE (ConE ConId) (LitE (Str "prana-test"))))
+                        (AppE (ConE ConId) (LitE (Str "Id")))
+                  }
+              ])
+          , ( "On"
+            , [ Bind
+                  { bindVar = ExportedIndex 6611
+                  , bindExp =
+                      LamE
+                        (LocalIndex 57222)
+                        (LamE
+                           (LocalIndex 57223)
+                           (LamE
+                              (LocalIndex 57224)
+                              (LamE
+                                 (LocalIndex 57225)
+                                 (AppE
+                                    (AppE
+                                       (VarE (LocalIndex 57222))
+                                       (AppE
+                                          (VarE (LocalIndex 57223))
+                                          (VarE (LocalIndex 57224))))
+                                    (AppE
+                                       (VarE (LocalIndex 57223))
+                                       (VarE (LocalIndex 57225)))))))
+                  }
+              , Bind
+                  { bindVar = ExportedIndex 6612
+                  , bindExp =
+                      AppE
+                        (AppE
+                           (ConE ConId)
+                           (AppE (ConE ConId) (LitE (Str "prana-test"))))
+                        (AppE (ConE ConId) (LitE (Str "On")))
+                  }
+              ])
+          ])
+
+-- | Compile a single module.
+compileModule :: String -> String -> IO [Bind]
+compileModule name contents =
+  fmap (snd . head) (compileModules [(name, contents)])
 
 -- | Compile the given sources as a set of modules.
-compileModules :: [(String,String)] -> IO (Maybe [(String, ByteString)])
+compileModules :: [(String,String)] -> IO [(String, [Bind])]
 compileModules modules =
   withSystemTempDirectory
     "prana-compile"
     (\dir -> do
        let fps = map (\(moduleName, src) -> (moduleName ++ ".hs", src)) modules
        mapM_ (\(fp, src) -> writeFile (dir ++ "/" ++ fp) src) fps
-       code <- compileFile dir (map fst fps)
+       (code, out, err) <- compileFile dir (map fst fps)
        case code of
          ExitSuccess ->
-           fmap
-             sequence
-             (mapM
-                (\(moduleName, _) ->
-                   fmap
-                     (Just . (moduleName, ))
-                     (S.readFile
-                        (dir ++ "/prana-test_" ++ moduleName ++ ".prana")))
-                modules)
-         ExitFailure {} -> pure Nothing)
+           mapM
+             (\(moduleName, _) -> do
+                bytes <-
+                  L.readFile (dir ++ "/prana-test_" ++ moduleName ++ ".prana")
+                case runGetOrFail (decodeArray decodeBind) bytes of
+                  Left (_, pos, e) ->
+                    error
+                      ("Decoding error! " ++
+                       e ++
+                       " at index " ++
+                       show pos ++
+                       "\n\nFile:\n\n" ++
+                       show (L.unpack bytes) ++
+                       "\n\nAt index:\n\n" ++
+                       show (drop (fromIntegral pos - 1) (L.unpack bytes)))
+                  Right (_, _, binds) -> pure (moduleName, binds))
+             modules
+         ExitFailure {} -> error (unlines ["Compile failed:", out, err]))
 
 -- | Run a compile with docker in the given dir on the given file.
-compileFile :: FilePath -> [FilePath] -> IO ExitCode
+compileFile :: FilePath -> [FilePath] -> IO (ExitCode, String, String)
 compileFile pwd fps = do
-  rawSystem
+  readProcessWithExitCode
     "docker"
     ([ "run"
      , "-v" ++ pwd ++ ":" ++ pwd
@@ -54,5 +137,7 @@ compileFile pwd fps = do
      , "-fbyte-code"
      , "-this-unit-id"
      , "prana-test"
+     , "-v0"
      ] ++
      fps)
+    ""
