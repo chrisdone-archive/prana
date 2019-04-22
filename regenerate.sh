412624bafd194d5ef4b84b29928e97d2bdd80b50
Chris Done <chrisdone@gmail.com>
2019-04-22 14:13:07 +0100

    Add regenerate.sh for dev testing purposes

---
+++ regenerate.sh
@@ -0,0 +1,17 @@
+set -e
+set -x
+
+rm -r ~/Work/chrisdone/prana/prana-dir/
+mkdir -p ~/Work/chrisdone/prana/prana-dir/packages/
+
+cd ~/Work/chrisdone/prana/ghc-8.4/libraries/ghc-prim/
+
+PRANA_DIR=~/Work/chrisdone/prana/prana-dir/ PRANA_MODE=INSTALL time -p -- ./Setup build --ghc-options="-O0"
+
+cd ../integer-gmp/
+
+PRANA_DIR=~/Work/chrisdone/prana/prana-dir/ PRANA_MODE=INSTALL time -p -- ./Setup build --ghc-options="-O0"
+
+cd ../base-4.11.1.0/
+
+PRANA_DIR=~/Work/chrisdone/prana/prana-dir/ PRANA_MODE=INSTALL time -p -- ./Setup build --ghc-options="-O0"
