a549fb70d1b5fb8b438475578df23a8329380088
Chris Done <chrisdone@gmail.com>
2019-04-29 15:36:44 +0100

    Explain !!

--- prana-interpreter/test/assets/FibCodata.hs
+++ prana-interpreter/test/assets/FibCodata.hs
@@ -9,6 +9,10 @@ it = fibs FibCodata.!! 49
 fibs :: [Int]
 fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
 
+-- | We implement our own Integer-based !! to force use of
+-- MultiValAlts -- using unboxed tuples for a loop. The regular
+-- Prelude.!! works fine, but tests less of the interpreter than this
+-- implementation.
 (!!) :: [p] -> Integer -> p
 xs !! n
   | otherwise =
