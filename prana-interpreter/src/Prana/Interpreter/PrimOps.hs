4d22d781b6c825bce7a477641275088e096b394b
Chris Done <chrisdone@gmail.com>
2019-05-17 15:17:24 +0100

    Derived primops

--- prana-interpreter/src/Prana/Interpreter/PrimOps.hs
+++ prana-interpreter/src/Prana/Interpreter/PrimOps.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE EmptyCase #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE UnboxedTuples #-}
@@ -15,7 +16,7 @@ module Prana.Interpreter.PrimOps
   ) where
 
 import GHC.Exts
-import Prana.Interpreter.Boxing
+import Prana.Interpreter.PrimOps.TH
 import Prana.Interpreter.Types
 import Prana.Types
 
@@ -27,44 +28,16 @@ evalPrimOp ::
   -> PrimOpType
   -> IO Whnf
 evalPrimOp index evalSomeVarId primOp args typ =
-  case primOp of
-
-    -- Int# -> Int# -> (# Int#, Int# #)
-    IntSubCOp ->
-      case args of
-        [arg1, arg2] -> do
-          I# i <- evalIntArg evalSomeVarId arg1
-          I# i2 <- evalIntArg evalSomeVarId arg2
-          case subIntC# i i2 of
-            (# x#, y# #) -> do
-              xBox <- boxWhnf (LitWhnf (IntLit (I# x#)))
-              yBox <- boxWhnf (LitWhnf (IntLit (I# y#)))
-              pure (ConWhnf (UnboxedTupleConId 2) [xBox, yBox])
-        _ -> error ("Invalid arguments to IntSubCOp: " ++ show args)
-    -- IntEqOp -> $(op [| (==#) :: Int# -> Int# -> Int# |])
-    -- IntLtOp -> $(op [| (<#) :: Int# -> Int# -> Int# |])
-    -- IntAddOp -> $(op [| (+#) :: Int# -> Int# -> Int# |])
-    -- IntSubOp -> $(op [| (-#) :: Int# -> Int# -> Int# |])
-    -- IntNegOp -> $(op [| negateInt# :: Int# -> Int# |])
-    TagToEnumOp ->
-      case args of
-        [arg] -> do
-          (I# ii) <- evalIntArg evalSomeVarId arg
-          case typ of
-            BoolType -> do
-              let bool = tagToEnum# ii :: Bool
-                  !con =
-                    case bool of
-                      False -> reverseIndexFalse index
-                      True -> reverseIndexTrue index
-              pure (ConWhnf con [])
-            _ -> error "Unknown type for tagToEnum."
-        _ -> error ("Invalid arguments to TagToEnumOp: " ++ show args)
-
-    _ ->
-      error
-        ("Unimplemented primop: " ++
-         show primOp ++ " (type: " ++ show typ ++ "), args were: " ++ show args)
+  $(derivePrimOpsCase
+      Options
+        { optionsOp = 'primOp
+        , optionsArgs = 'args
+        , optionsEvalInt = 'evalIntArg
+        , optionsEvalSomeVarId = 'evalSomeVarId
+        , optionsManualImplementations = [('TagToEnumOp, 'tagToEnum)]
+        , optionsType = 'typ
+        , optionsIndex = 'index
+        })
 
 evalIntArg :: (SomeVarId -> IO Whnf) -> Arg -> IO Int
 evalIntArg evalSomeVarId =
@@ -80,3 +53,19 @@ evalIntArg evalSomeVarId =
           error
             ("Unexpected whnf for evalIntArg (I'm sure ClosureWhnf will come up here): " ++
              show whnf)
+
+tagToEnum :: ReverseIndex -> PrimOpType -> (SomeVarId -> IO Whnf) -> [Arg] -> IO Whnf
+tagToEnum index typ evalSomeVarId args =
+  case args of
+    [arg] -> do
+      (I# ii) <- evalIntArg evalSomeVarId arg
+      case typ of
+        BoolType -> do
+          let bool = tagToEnum# ii :: Bool
+              !con =
+                case bool of
+                  False -> reverseIndexFalse index
+                  True -> reverseIndexTrue index
+          pure (ConWhnf con [])
+        _ -> error "Unknown type for tagToEnum."
+    _ -> error ("Invalid arguments to TagToEnumOp: " ++ show args)
---
+++ prana-interpreter/src/Prana/Interpreter/PrimOps/TH.hs
@@ -0,0 +1,163 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TemplateHaskellQuotes #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE UnboxedTuples #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+-- |
+
+module Prana.Interpreter.PrimOps.TH where
+
+import Data.List
+import Data.Maybe
+import GHC.Exts
+import Language.Haskell.TH
+import Prana.Interpreter.Types
+import Prana.PrimOp (ty, name, Entry(..), parsePrimops, Ty(..), TyCon(..))
+import Prana.Types (Lit(..))
+
+data Options =
+  Options
+    { optionsOp :: !Name
+    , optionsArgs :: !Name
+    , optionsEvalInt :: !Name
+    , optionsIndex :: !Name
+    , optionsType :: !Name
+    , optionsEvalSomeVarId :: !Name
+    , optionsManualImplementations :: [(Name, Name)]
+    }
+
+derivePrimOpsCase :: Options -> Q Exp
+derivePrimOpsCase options = do
+  entries <- runIO parsePrimops
+  derived <-
+    fmap
+      catMaybes
+      (mapM
+         (\case
+            PrimOpSpec {cons, name, ty} -> do
+              case derivePrimOpAlt options name ty of
+                Left {} -> do
+                  pure Nothing
+                Right expr ->
+                  pure (Just (match (conP (mkName cons) []) (normalB expr) []))
+            _ -> pure Nothing)
+         entries)
+  let manuals =
+        map
+          (\(conName, funName) ->
+             match
+               (conP conName [])
+               (normalB
+                  (appE
+                     (appE
+                        (appE
+                           (appE (varE funName) (varE (optionsIndex options)))
+                           (varE (optionsType options)))
+                        (varE (optionsEvalSomeVarId options)))
+                     (varE (optionsArgs options))))
+               [])
+          (optionsManualImplementations options)
+  caseE (varE (optionsOp options)) (derived <> manuals <> [def])
+  where
+    def =
+      match
+        wildP
+        (normalB
+           (appE (varE 'error) (appE (varE 'show) (varE (optionsOp options)))))
+        []
+
+derivePrimOpAlt :: Options -> String -> Ty -> Either String (Q Exp)
+derivePrimOpAlt options primName ty = do
+  bindings <-
+    mapM
+      (unwrapArg primName options)
+      (zip resultNames (zip argNames (primArgTys ty)))
+  retExpr <- wrapResult primName resultName ty
+  pure
+    (caseE
+       (varE (optionsArgs options))
+       [ match
+           (listP (map varP argNames))
+           (normalB
+              (doE
+                 (concat
+                    [ bindings
+                    , [ letS
+                          [ valD
+                              (bangP (varP resultName))
+                              (normalB
+                                 (foldl'
+                                    appE
+                                    (varE (mkName primName))
+                                    (map varE resultNames)))
+                              []
+                          ]
+                      , retExpr
+                      ]
+                    ])))
+           []
+       , match
+           wildP
+           (normalB
+              (appE
+                 (varE 'error)
+                 (stringE ("Invalid arguments to primop: " ++ show primName))))
+           []
+       ])
+  where
+    resultName = mkName "result"
+    argNames = zipWith argName [0 ..] (primArgTys ty)
+    resultNames = zipWith mkresultName [0 ..] (primArgTys ty)
+    argName :: Int -> a -> Name
+    argName i _ = mkName ("wrapped_arg_" ++ show i)
+    mkresultName :: Int -> a -> Name
+    mkresultName i _ = mkName ("arg_unwrapped_" ++ show i)
+
+wrapResult :: [Char] -> Name -> Ty -> Either [Char] StmtQ
+wrapResult primName resultName ty =
+  case primReturnTy ty of
+    Just (TyApp (TyCon "Int#") []) ->
+      pure
+        (noBindS
+           (appE
+              (varE 'pure)
+              (appE
+                 (conE 'LitWhnf)
+                 (appE (conE 'IntLit) (appE (conE 'I#) (varE resultName))))))
+    Just retTy -> Left (primName ++ ": Unknown return type " ++ show retTy)
+    Nothing -> Left (primName ++ ": Couldn't find return type of " ++ show ty)
+
+unwrapArg :: [Char] -> Options -> (Name, (Name, Ty)) -> Either [Char] StmtQ
+unwrapArg primName options =
+  \(result, (arg, argTy)) ->
+    case argTy of
+      TyApp (TyCon "Int#") [] ->
+        pure
+          (bindS
+             (conP 'I# [varP result])
+             (appE
+                (appE
+                   (varE (optionsEvalInt options))
+                   (varE (optionsEvalSomeVarId options)))
+                (varE arg)))
+      _ -> Left (primName ++ ": Unknown arg type: " ++ show argTy)
+
+primArgTys :: Ty -> [Ty]
+primArgTys =
+  \case
+    TyF arg rest -> arg : primArgTys rest
+    _ -> []
+
+primReturnTy :: Ty -> Maybe Ty
+primReturnTy =
+  \case
+    TyF _ rest -> primReturnTy rest
+    ty -> Just ty
